b9cd2fa431a5125d88814da5550232663c1a22b0
BOTELHO <douglasbotelho2@gmail.com>
2026-08-11 01:20:41 +0000

    adicionar scripts `lint:check e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
- return <h1>Renatim,my friend es pato en cs1.6</h1>
+  return <h1>Renatim,my friend es pato en cs1.6</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+
+    console.log('indentação errada')
 
 export default Home;
